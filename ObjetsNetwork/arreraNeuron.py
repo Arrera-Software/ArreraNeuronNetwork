2ab07066d4e8de9a56483404a7943999815f7362
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2023-07-21 13:44:12 +0200

    Ajout de carractere a remplacer

--- ObjetsNetwork/arreraNeuron.py
+++ ObjetsNetwork/arreraNeuron.py
@@ -33,6 +33,10 @@ class ArreraNetwork :
         chaine.replace('"'," ")
         chaine.replace("_"," ")
         chaine.replace('/'," ")
+        chaine.replace("é","e")
+        chaine.replace("è","e")
+        chaine.replace("à","a")
+        chaine.replace("ç","c")
         return chaine
     
     def neuron(self,var:str) :
