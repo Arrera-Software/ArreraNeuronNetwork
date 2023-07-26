99c99ea8552d38a0e04fbfe068a9fb4b64662da5
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2023-07-26 13:44:12 +0200

    Ajout d'une librairy pour parler et manipuler les fichier JSON

---
+++ librairy/travailJSON.py
@@ -0,0 +1,18 @@
+import json
+
+class jsonWork : 
+    def __init__(self,file):
+        self.fichier = file
+        
+    def lectureJSON(self,flag): # Permet de lire la valeur du flag defini a l'appel de la fonction
+        with open(self.fichier, 'r' , encoding='utf-8') as openfile:
+            dict = json.load(openfile)[flag]
+        return str(dict)
+     
+    def EcritureJSON(self,flag,valeur):#Permet d'ecrire une nouvelle valeur a flag definie
+        openfile = open(self.fichier, 'r' , encoding='utf-8')
+        dict = json.load(openfile)
+        openfile.close()
+        writeFile = open(self.fichier, 'w', encoding='utf-8')
+        dict[flag] = valeur
+        json.dump(dict,writeFile,indent=2)
