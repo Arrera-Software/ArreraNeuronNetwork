969112e7c5e4ad3884f7d241f3aaa12be096e652
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2023-07-26 22:42:10 +0200

    Ajout de la fonction de calcule simple

--- neuron/neuronMain.py
+++ neuron/neuronMain.py
@@ -6,7 +6,7 @@ class neuroneMain :
         self.gestionNeuron = gestionnaire
         self.fonctionArreraNetwork = fncArreraNetwork
         
-    def neurone(self,requette):
+    def neurone(self,requette:str):
         #Initilisation des variable nbRand et text et valeur
         nbRand = 0
         text = ""
@@ -24,6 +24,21 @@ class neuroneMain :
         #reponse du neuron main
         if "lire un truc" in requette or  "lit un truc" in requette :
             text = self.fonctionArreraNetwork.reading()
+        else :
+            if "calcule" in requette :
+                requette = requette.replace("calcule","")
+                requette = requette.replace(" ","")
+                if "1" in requette or "2" in requette  or "3" in requette or "4" in requette or "5" in requette or "6" in requette  or "7" in requette or "8" in requette or "9" in requette or "0" in requette and "+" in requette or "-" in requette or "*" in requette or "/" in requette :
+                    resultat =  eval(requette)
+                    if self.etatVous == True :
+                        text = "Voici le resultat de votre calcule "+self.genre+" est "+str(resultat)
+                    else :
+                        text = "Voici le resultat de ton calcule "+self.user+" est "+str(resultat)
+                else :
+                    if self.etatVous == True :
+                        text = "Le calcule que vous me demander de faire "+self.genre+" est imposible a faire."
+                    else :
+                        text = "Le calcule que tu me demande de faire est imposible."
         #Mise a jour de la valeur                                                               
         valeur = self.gestionNeuron.verrifSortie(text)
         #Sauvegarde des sortie                         
