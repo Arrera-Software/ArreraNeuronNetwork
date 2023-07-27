9b7da43c8fce4a3c55d87e94c7eb5d39b82ddc40
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2023-07-27 22:14:10 +0200

    Ajout de la fonction d'actualites

--- arreraSoftware/fncArreraNetwork.py
+++ arreraSoftware/fncArreraNetwork.py
@@ -1,7 +1,9 @@
 from tkinter import*
+import random
 from librairy.travailJSON import *
 from ObjetsNetwork.gestion import*
 from arreraSoftware.fonctionLecture import *
+from arreraSoftware.fonctionMeteoActu import *
 
 class fncArreraNetwork:
     def __init__(self,json:jsonWork,gestionNeuron:gestionNetwork,iconAssistant:str):
@@ -16,6 +18,8 @@ class fncArreraNetwork:
         self.name = self.gestionNeuron.getName()
         self.user = self.gestionNeuron.getUser()
         self.genre = self.gestionNeuron.getGenre()
+        #initialisation objet 
+        self.actuMeteo = MeteoActu("3b43e18afcf945888748071d177b8513","6","fr","fr")
         
     def reading(self):
         fncLecture(self.travailJSON,self.name,self.icon)
@@ -24,4 +28,14 @@ class fncArreraNetwork:
         else :
             text = "J'espere que sa ta servie "+self.name
         return text
-    
+
+    def sortieActualités(self):
+        listActu = self.actuMeteo.Actu()
+        nbrand1 = random.randint(0,1)
+        nbrand2 = random.randint(2,3)
+        nbrand3 = random.randint(4,5)
+        if self.etatVous == True :
+            text = "Les actualites du jour sont "+listActu[nbrand1]+" , "+listActu[nbrand2] + " et "+listActu[nbrand3]+"."
+        else :
+            text = "Les news du jour sont "+listActu[nbrand1]+" , "+listActu[nbrand2] + " et "+listActu[nbrand3]+"."
+        return str(text)
---
+++ arreraSoftware/fonctionMeteoActu.py
@@ -0,0 +1,17 @@
+import requests
+
+class MeteoActu :
+    def __init__(self,keyActu:str,nbPage:int,pay:str,lang:str):
+        self.nbPage = int(nbPage)
+        self.pay = pay
+        self.keyActu = keyActu
+        self.langue = lang
+        self.URLActu = "https://newsapi.org/v2/top-headlines?"+"apiKey="+self.keyActu+"&country="+self.pay+"&category=general"+"&pageSize="+str(self.nbPage)+"&language="+self.langue
+        
+    def Actu(self):
+        retourActu = requests.get(self.URLActu).json()["articles"]
+        listeDescription = []
+        for i in range(0,self.nbPage) :
+            listeDescription.append(retourActu[i]["title"])
+        
+        return listeDescription
