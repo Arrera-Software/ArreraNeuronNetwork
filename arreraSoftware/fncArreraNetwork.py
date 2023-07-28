51cd121b805c967fed44e81f1a5c764ca09e9c9d
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2023-07-28 18:36:20 +0200

    Ajour de l'initialisation de l'objet GPS et Meto + Ajout de la fonction de sortieMeteo

--- arreraSoftware/fncArreraNetwork.py
+++ arreraSoftware/fncArreraNetwork.py
@@ -4,6 +4,7 @@ from librairy.travailJSON import *
 from ObjetsNetwork.gestion import*
 from arreraSoftware.fonctionLecture import *
 from arreraSoftware.fonctionMeteoActu import *
+from arreraSoftware.fonctionGPS import*
 
 class fncArreraNetwork:
     def __init__(self,json:jsonWork,gestionNeuron:gestionNetwork,iconAssistant:str):
@@ -20,6 +21,8 @@ class fncArreraNetwork:
         self.genre = self.gestionNeuron.getGenre()
         #initialisation objet 
         self.actu = Actu("3b43e18afcf945888748071d177b8513","6","fr","fr")
+        self.gps = GPS("19bfbee6112be5b3d9a64d4ccec72602")
+        self.meteo = Meteo("19bfbee6112be5b3d9a64d4ccec72602")
         
     def reading(self):
         fncLecture(self.travailJSON,self.name,self.icon)
@@ -39,3 +42,64 @@ class fncArreraNetwork:
         else :
             text = "Les news du jour sont "+listActu[nbrand1]+" , "+listActu[nbrand2] + " et "+listActu[nbrand3]+"."
         return str(text)
+    
+    def sortieMeteo(self,ville):
+        if ville == "" :
+            sortieGPS = self.gps.recuperationCordonneePossition()
+            if sortieGPS == True :
+                sortieGPS = self.gps.recuperationNameVillePosition()
+                if sortieGPS == True :
+                    nameVille = self.gps.getNameVille()
+                    lon = self.gps.getlonPossition()
+                    lat = self.gps.getlatPossition()
+                    sortiMeteo = self.meteo.recuperationDataMeteo(lat,lon)
+                    if sortiMeteo == True :
+                        if self.etatVous == True :
+                            nbrand = random.randint(0,1)
+                            listReponse = ["La meteo a "+nameVille+" est "+self.meteo.getdescription()+" avec une température de "+self.meteo.gettemperature()+" °C",
+                                            "La meteo a votre localisation est "+self.meteo.getdescription()+" avec une température de "+self.meteo.gettemperature()+" °C"]
+                            text =  listReponse[nbrand]
+                        else :
+                            nbrand = random.randint(0,1)
+                            listReponse = ["La meteo a "+nameVille+" est "+self.meteo.getdescription()+" avec une température de "+self.meteo.gettemperature()+" °C",
+                                            "La meteo a ta localisation est de "+self.meteo.getdescription()+" avec une température de "+self.meteo.gettemperature()+" °C"]
+                        
+                            text =  listReponse[nbrand]
+                    else :
+                        if self.etatVous == True :
+                            text = "Je suis désoler "+self.genre+" "+self.user+". Mais je ne parvien pas a recuperer la meteo"
+                        else :
+                            text = "Je suis désoler "+self.user+" je n'arrive pas a savoir la meteo."  
+                else :
+                        if self.etatVous == True :
+                            text = "Je suis désoler "+self.genre+" "+self.user+". Mais je ne parvien pas a recuperer la meteo"
+                        else :
+                            text = "Je suis désoler "+self.user+" je n'arrive pas a savoir la meteo."
+            else :
+                        if self.etatVous == True :
+                            text = "Je suis désoler "+self.genre+" "+self.user+". Mais je ne parvien pas a recuperer la meteo"
+                        else :
+                            text = "Je suis désoler "+self.user+" je n'arrive pas a savoir la meteo."
+        else :
+            sortieGPS = self.gps.recuperationCordonneeVille(ville)
+            if sortieGPS == True :
+                lat = self.gps.getlatVille()
+                lon = self.gps.getLonVille()
+                sortiMeteo = self.meteo.recuperationDataMeteo(lat,lon)
+                if  sortiMeteo == True:
+                    nameVille =  ville
+                    if self.etatVous == True :
+                        text= "La meteo a "+nameVille+" est "+self.meteo.getdescription()+" avec une température de "+self.meteo.gettemperature()+" °C"             
+                    else :
+                        text = "La meteo a "+nameVille+" est "+self.meteo.getdescription()+" avec une température de "+self.meteo.gettemperature()+" °C"            
+                else :
+                        if self.etatVous == True :
+                            text = "Je suis désoler "+self.genre+" "+self.user+". Mais je ne parvien pas a recuperer la meteo"
+                        else :
+                            text = "Je suis désoler "+self.user+" je n'arrive pas a savoir la meteo."  
+            else :
+                if self.etatVous == True :
+                    text = "Je suis désoler "+self.genre+" "+self.user+". Mais je ne parvien pas a recuperer la meteo"
+                else :
+                    text = "Je suis désoler "+self.user+" je n'arrive pas a savoir la meteo."  
+        return text   
