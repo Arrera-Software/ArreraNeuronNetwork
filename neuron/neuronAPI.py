a1a317946deda3df11ea61bd1b052eaa827f3d26
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2023-08-02 20:23:57 +0200

    Fonctionnalieter GPS et choississant sa destination et depart

--- neuron/neuronAPI.py
+++ neuron/neuronAPI.py
@@ -9,15 +9,17 @@ class neuroneAPI :
         self.fonctionArreraNetwork = fncArreraNetwork
         self.etatVilleDomicile = self.gestionNeuron.getEtatLieuDomicile()
         self.etatVilleTravail = self.gestionNeuron.getEtatLieuTravail()
+        self.villeGPS1 = ""
+        self.villeGPS2 = ""
         
-    def neurone(self,requette:str):
+    def neurone(self,requette:str,oldSortie:str,oldRequette:str):
         #Initilisation des variable nbRand et text et valeur
         nbRand = 0
         text = ""
         valeur = 0
         #Recuperation atribut de l'assistant
-        self.oldrequette = self.gestionNeuron.getOldrequette()
-        self.oldsortie = self.gestionNeuron.getOldSortie()
+        self.oldrequette = oldRequette
+        self.oldsortie = oldSortie
         self.nbDiscution = self.gestionNeuron.getNbDiscution()
         self.name = self.gestionNeuron.getName()
         self.etatVous = self.gestionNeuron.getVous()
@@ -61,19 +63,53 @@ class neuroneAPI :
                     else :
                         if "itineraire" in requette or "comment aller" in requette :
                             sortieFnc = False
-                            if "a" in requette or "au" in requette :
-                                loc2 = requette.replace("comment","")
-                                loc2 = requette.replace("aller","")
-                                loc2 = requette.replace("trouve-moi","")
-                                loc2 = requette.replace("trouve","")
-                                loc2 = requette.replace("moi","")
-                                if "domicile" in requette or "residence" in requette or "maison" in requette or "appartement" in requette or "chez moi" in requette or "foyer" in requette or "maison" in requette or "foyer" in requette or "demeure "in requette :
-                                    sortieFnc = self.fonctionArreraNetwork.sortieItineraires("loc",self.gestionNeuron.getValeurfichierUtilisateur("lieuDomicile"))
-                                else :
-                                    if "bureau" in requette or "lieu de travail" in requette or "entreprise" in requette or "societe" in requette or "boulot" in requette or "cabinet" in requette or "college" in requette or "lycee" in requette or "ecole" in requette or "campus" in requette or "universite" in requette :
-                                        sortieFnc = self.fonctionArreraNetwork.sortieItineraires("loc",self.gestionNeuron.getValeurfichierUtilisateur("lieuTravail"))
+                            etatde = False
+                            if "de" in requette :
+                                loc = requette.replace("comment","")
+                                loc = requette.replace("aller","")
+                                loc = requette.replace("trouve-moi","")
+                                loc = requette.replace("trouve","")
+                                loc = requette.replace("moi","")
+                                self.villeGPS1 = loc
+                                etatde = True
+                            else :
+                                if "a" in requette or "au" in requette :
+                                    loc = requette.replace("comment","")
+                                    loc = requette.replace("aller","")
+                                    loc = requette.replace("trouve-moi","")
+                                    loc = requette.replace("trouve","")
+                                    loc = requette.replace("moi","")
+                                    if "domicile" in requette or "residence" in requette or "maison" in requette or "appartement" in requette or "chez moi" in requette or "foyer" in requette or "maison" in requette or "foyer" in requette or "demeure "in requette :
+                                        sortieFnc = self.fonctionArreraNetwork.sortieItineraires("loc",self.gestionNeuron.getValeurfichierUtilisateur("lieuDomicile"))
                                     else :
-                                        sortieFnc= self.fonctionArreraNetwork.sortieItineraires("loc",loc2)
+                                        if "bureau" in requette or "lieu de travail" in requette or "entreprise" in requette or "societe" in requette or "boulot" in requette or "cabinet" in requette or "college" in requette or "lycee" in requette or "ecole" in requette or "campus" in requette or "universite" in requette :
+                                            sortieFnc = self.fonctionArreraNetwork.sortieItineraires("loc",self.gestionNeuron.getValeurfichierUtilisateur("lieuTravail"))
+                                        else :
+                                            sortieFnc= self.fonctionArreraNetwork.sortieItineraires("loc",loc)
+                            if sortieFnc== True :
+                                if self.etatVous == True :
+                                    text = "J'espére que sa vous aidera "+self.genre+" "+self.user
+                                else :
+                                    text ="Voila "+self.user
+                            else :
+                                if etatde == True :
+                                    if self.etatVous == True :
+                                        text = "Quelle est votre destination "+self.genre
+                                    else :
+                                        text = "Quelle est ta destination final "+self.user
+                                else :
+                                    if self.etatVous == True :
+                                        text = "Je suis desoler "+self.genre+" "+self.user+" mais je subis un probleme qui m'empeche de vous montrer l'itinéraire"
+                                    else :
+                                        text ="Desoler"+self.user+" Je ne peux pas te fournir ton itinéraire"
+                        if "Quelle est votre destination" in self.oldsortie or "Quelle est ta destination final" in self.oldsortie :
+                            sortieFnc = False
+                            loc = requette.replace("comment","")
+                            loc = requette.replace("aller","")
+                            loc = requette.replace("trouve-moi","")
+                            loc = requette.replace("trouve","")
+                            loc = requette.replace("moi","")
+                            sortieFnc = self.fonctionArreraNetwork.sortieItineraires(self.villeGPS1,loc)
                             if sortieFnc== True :
                                 if self.etatVous == True :
                                     text = "J'espére que sa vous aidera "+self.genre+" "+self.user
@@ -85,12 +121,6 @@ class neuroneAPI :
                                 else :
                                     text ="Desoler"+self.user+" Je ne peux pas te fournir ton itinéraire"
                             
-                            
         #Mise a jour de la valeur                                                               
         valeur = self.gestionNeuron.verrifSortie(text)
-        #Sauvegarde des sortie                         
-        self.gestionNeuron.setHistory(text,requette)
-        #Ajout d'une discution
-        self.gestionNeuron.addDiscution()
-        #Retour des valeur
         return valeur , text
