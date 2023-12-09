a51e1275244e822d1da62ea6e8bfc0e4414cf332
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2023-12-09 16:00:06 +0100

    arreter dans les option pour arrter l'assistant

--- ObjetsNetwork/arreraNeuron.py
+++ ObjetsNetwork/arreraNeuron.py
@@ -95,7 +95,7 @@ class ArreraNetwork :
                                 else :
                                     valeur = 0
                                 if valeur == 0 :
-                                    if "stop" in requette or "au revoir" in requette or "quitter" in requette or "bonne nuit" in requette or "adieu" in requette or "bonne soirée" in requette :
+                                    if "stop" in requette or "au revoir" in requette or "quitter" in requette or "bonne nuit" in requette or "adieu" in requette or "bonne soirée" in requette or "arreter" in requette :
                                         text = self.__formuleNeuron.aurevoir(datetime.datetime.now().hour)
                                         valeur = 15
                                     else : 
