d2186a8075d1d1519cd796ef230b0229c9095217
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2024-07-12 15:50:47 +0200

    Ajout historique a API

--- neuron/API.py
+++ neuron/API.py
@@ -43,9 +43,11 @@ class neuroneAPI :
             #reponse du neuron main
             if (("resumer actualites" in requette) or ("resumer actu" in requette)) :
                 self.__valeurOut,self.__listSortie = self.__fonctionArreraNetwork.sortieResumerActualite()
+                self.__objHistorique.setAction("Resumer actualiter")
             else :
                 if "actualites" in requette or "actu" in requette:
                     self.__valeurOut,self.__listSortie = self.__fonctionArreraNetwork.sortieActualités()
+                    self.__objHistorique.setAction("Actualités")
                 else :
                     if "meteo" in requette :
                         nb = self.__gestionNeuron.getnbVilleMeteo()
@@ -71,6 +73,7 @@ class neuroneAPI :
                                             self.__valeurOut,self.__listSortie = self.__fonctionArreraNetwork.sortieMeteoTowmorNoon("")
                                 else :    
                                     self.__valeurOut,self.__listSortie = self.__fonctionArreraNetwork.sortieMeteoTowmorNoon("")
+                            self.__objHistorique.setAction("Meteo demain midi")
                         else :
                             if (("demain matin" in requette) or ("demain" in requette)):
                                 for i in range(0,nb):
@@ -92,11 +95,13 @@ class neuroneAPI :
                                                 self.__valeurOut,self.__listSortie = self.__fonctionArreraNetwork.sortieMeteoTowmoroMorning("")
                                     else :    
                                         self.__valeurOut,self.__listSortie = self.__fonctionArreraNetwork.sortieMeteoTowmoroMorning("")
+                                    self.__objHistorique.setAction("Meteo demain matin")
                             else :
                                 for i in range(0,nb):
                                     ville = chaine.netoyage(villes[i])
                                     if ville in requette :
                                         self.__valeurOut,self.__listSortie = self.__fonctionArreraNetwork.sortieMeteoToday(villes[i])
+                                        self.__objHistorique.setAction("Meteo ajourd'hui dans "+ville[i])
                                         resultat = 1
                                         break
                                     else :
@@ -105,24 +110,32 @@ class neuroneAPI :
                                     if self.__etatVilleDomicile == True or self.__etatVilleTravail == True : 
                                         if "domicile" in requette or "residence" in requette or "maison" in requette or "appartement" in requette or "chez moi" in requette or "foyer" in requette or "maison" in requette or "foyer" in requette or "demeure "in requette :
                                             self.__valeurOut,self.__listSortie = self.__fonctionArreraNetwork.sortieMeteoToday(self.__gestionNeuron.getValeurfichierUtilisateur("lieuDomicile"))
+                                            self.__objHistorique.setAction("Meteo ajourd'hui au domicile")
                                         else :
                                             if "bureau" in requette or "lieu de travail" in requette or "entreprise" in requette or "societe" in requette or "boulot" in requette or "cabinet" in requette or "college" in requette or "lycee" in requette or "ecole" in requette or "campus" in requette or "universite" in requette :
                                                 self.__valeurOut,self.__listSortie = self.__fonctionArreraNetwork.sortieMeteoToday(self.__gestionNeuron.getValeurfichierUtilisateur("lieuTravail"))
+                                                self.__objHistorique.setAction("Meteo ajourd'hui au lieu de travail")
                                             else :
                                                 self.__valeurOut,self.__listSortie = self.__fonctionArreraNetwork.sortieMeteoToday("")
+                                                self.__objHistorique.setAction("Meteo ajourd'hui a la localisation")
                                     else :    
                                         self.__valeurOut,self.__listSortie = self.__fonctionArreraNetwork.sortieMeteoToday("")
+                                        self.__objHistorique.setAction("Meteo ajourd'hui a la localisation")
+
                     else :
                         if "temperature" in requette :
                             self.__valeurOut,self.__listSortie = self.__fonctionArreraNetwork.sortieTemperature()
+                            self.__objHistorique.setAction("Temperature")
                         else :
                             if "coordonnee gps" in requette or "position gps" in requette :
                                 self.__valeurOut,self.__listSortie = self.__fonctionArreraNetwork.sortieGPS()
+                                self.__objHistorique.setAction("Possition gps")
                             else :
                                 if ((("indique moi l'itineraire de" in requette)and("sur le gps" in requette))
                                     or(("lance le gps pour un trajet de" in requette )and("a" in requette))):
                                     self.__listSortie = [self.__fonctionArreraNetwork.sortieItineraire(requette),""]
                                     self.__valeurOut = 1
+                                    self.__objHistorique.setAction("Ouverture d'un itineraire sur google map")
                                 else :
                                     if ("gps aide" in requette):
                                         if (etatVous == True):
@@ -132,6 +145,7 @@ class neuroneAPI :
                                             if "traduis" in requette or "traduction" in requette or "traduire" in requette :
                                                 chaineCarractere = str(requette).lower()
                                                 presenceLang = False
+                                                self.__objHistorique.setAction("Outil de traduction")
                                                 for i in range(0,len(self.__listeLang)-1):
                                                     if self.__listeLang[i] in chaineCarractere :
                                                         presenceLang = True
