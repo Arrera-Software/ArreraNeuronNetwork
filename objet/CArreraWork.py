c9ec7737f36177ce844e74da721776c472ba4315
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2024-07-14 22:38:28 +0200

    Commencement d'Arrera Work

---
+++ objet/CArreraWork.py
@@ -0,0 +1,104 @@
+from tkinter import filedialog 
+from tkinter.messagebox import*
+from objet.arreradocument import*
+from objet.arreratableur import*
+
+class CArreraWork :
+    def __init__(self):
+        # Variable etat ouverture fichier
+        self.__tableurOpen = False
+        self.__wordOpen = False 
+        # Varriable des objet 
+        self.__objTableur = None
+        self.__objWord = None
+
+    def openTableur(self) : 
+        if (self.__tableurOpen==False):
+            # Demande de l'emplacement du fichier
+            showinfo("Work","Choisissez votre fichier exel")
+            emplacementFile = filedialog.asksaveasfilename(
+                    defaultextension='.xlsx', 
+                    filetypes=[("Fichiers Excel", "*.xlsx")])
+            if (emplacementFile == ""):
+                showwarning("Work","Aucun fichier selectionner")
+                return False
+            else :
+                self.__objTableur = CArreraTableur(emplacementFile)
+                showinfo("Work","Exel ouvert")
+                self.__tableurOpen = True
+                return True
+        else :
+            return False   
+
+    def openWord(self):
+        if (self.__wordOpen == False):
+            # Demande de l'emplacement du fichier
+            showinfo("Work","Choisissez votre fichier exel")
+            emplacementFile = filedialog.asksaveasfilename(
+                    defaultextension='.xlsx', 
+                    filetypes=[('Fichiers Word', '*.docx')])
+            if (emplacementFile == ""):
+                showwarning("Work","Aucun fichier selectionner")
+                return False
+            else :
+                self.__objWord = CArreraDocx(emplacementFile)
+                showinfo("Work","Exel ouvert")
+                self.__wordOpen = True
+                return True
+        else :
+            return False       
+
+    def setFormuleTableur(self,mode:int,case1:str,case2:str,caseDest:str):
+        """
+        1: Somme
+        2: Moyenne
+        3: Comptage
+        4: Minimun
+        5: Maximun
+        """
+        if (self.__tableurOpen==True):
+            match mode :
+                case 1 :
+                    self.__objTableur.somme(caseDest,case1,case1)
+                    self.__objTableur.saveFile()
+                    return True
+                case 2 :
+                    self.__objTableur.moyenne(caseDest,case1,case2)
+                    self.__objTableur.saveFile()
+                    return True
+                case 3 : 
+                    self.__objTableur.comptage(caseDest,case1,case2)
+                    self.__objTableur.saveFile()
+                    return True
+                case 4 :
+                    self.__objTableur.minimun(caseDest,case1,case2)
+                    self.__objTableur.saveFile()
+                    return True
+                case 5 :
+                    self.__objTableur.maximun(caseDest,case1,case2)
+                    self.__objTableur.saveFile()
+                    return True
+                case other :
+                    return False
+        else :
+            return False
+        
+    def setValeurTableur(self,case:str,valeur):
+        if ((self.__tableurOpen==True) and (case != "")):
+            self.__objTableur.write(case,valeur)
+            self.__objTableur.saveFile()
+            return True
+        else :
+            return False
+    
+    def closeTableur(self):
+        if (self.__tableurOpen==True):
+            self.__objTableur.saveFile()
+            self.__objTableur.closeFile()
+            del self.__objTableur
+            self.__objTableur = None
+            return True
+        else :
+            return False
+
+
---
+++ objet/arreradocument.py
@@ -0,0 +1,56 @@
+from docx import Document
+from odf import text, opendocument ,teletype
+from odf.opendocument import load
+
+class CArreraDocx :
+    def __init__(self,fileName:str) :
+        if ("docx" in fileName) :
+            self.__document = Document()
+            self.__fileName = fileName
+            self.__typeFile = "docx"
+            self.__init = True
+        else :
+            if ("odt"in fileName):
+                self.__document = opendocument.OpenDocumentText()
+                self.__fileName = fileName
+                self.__typeFile = "odt"
+                self.__init = True
+            else :
+                self.__fileName = fileName
+                self.__init = False
+    
+    def write(self,content:str):
+        if (self.__init==True):
+            if (self.__typeFile=="docx"):
+                for line in content.split('\n'):
+                    self.__document.add_paragraph(line)
+                self.__document.save(self.__fileName)
+                return True
+            else :
+                if (self.__typeFile=="odt"):
+                    for line in content.split('\n'):
+                        p = text.P()
+                        p.addText(line)
+                        self.__document.text.addElement(p)
+                    self.__document.save(self.__fileName)
+                    return True
+        else :
+            return False
+    
+    def read(self):
+        if (self.__init==True):
+            if (self.__typeFile=="docx"):
+                self.__document = Document(self.__fileName)
+                texte = []
+                for paragraph in self.__document.paragraphs:
+                    texte.append(paragraph.text)
+                return '\n'.join(texte)
+            else :
+                if (self.__typeFile=="odt"):
+                    self.__document = load(self.__fileName)
+                    content = ""
+                    for para in self.__document.getElementsByType(text.P):
+                        content += teletype.extractText(para)
+                    return content
+        else :
+            return "error"
