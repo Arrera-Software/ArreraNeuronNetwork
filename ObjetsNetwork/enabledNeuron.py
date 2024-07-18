429730a08873a042577ee23e09e0c418ce6c1fab
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2024-07-18 22:16:24 +0200

    Ajout du neuron work dans la gestion

--- ObjetsNetwork/enabledNeuron.py
+++ ObjetsNetwork/enabledNeuron.py
@@ -10,6 +10,7 @@ class GestArreraNeuron :
         self.__etatChatbot = configNeuron.lectureJSON("etatChatbot")
         self.__etatApi = configNeuron.lectureJSON("etatApi")
         self.__etatCodehelp = configNeuron.lectureJSON("etatCodehelp")
+        self.__etatWork = configNeuron.lectureJSON("etatWork")
         
     def getService(self):
         if self.__etatService == "1" :
@@ -55,6 +56,12 @@ class GestArreraNeuron :
     
     def getCodeHelp(self):
         if (self.__etatCodehelp=="1"):
+            return True
+        else :
+            return False
+    
+    def getWork(self):
+        if (self.__etatWork == "1"):
             return True
         else :
             return False
