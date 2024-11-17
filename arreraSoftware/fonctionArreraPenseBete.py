032047b19aa74c40420630ad74d78e8344ce554a
Baptiste P <baptiste.pauchet13@gmail.com>
2024-11-17 11:39:44 +0100

    Commencement affichage correct du contenu du fichier

--- arreraSoftware/fonctionArreraPenseBete.py
+++ arreraSoftware/fonctionArreraPenseBete.py
@@ -2,6 +2,8 @@ from tkinter import *
 from librairy.travailJSON import*
 from tkinter import filedialog 
 from tkinter import messagebox
+import markdown
+from tkhtmlview import HTMLLabel
 
 class fncArreraPostite:
     def __init__(self,fileConf:jsonWork):
@@ -28,12 +30,8 @@ class fncArreraPostite:
         frame.grid_columnconfigure(0, weight=1)
         
         # Zone de texte pour le post-it avec fond jaune
-        self.zoneTexte = Text(frame, bg="lightyellow", font=("Arial", 20))
+        self.zoneTexte = HTMLLabel(frame)
         self.zoneTexte.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
-        
-        # Création du bouton "Enregistrer"
-        btnSave = Button(self.__penseBete, text="Enregistrer", font=("Arial", 14),bg=self.__color,fg=self.__textColor,command=self.__saveFile)
-        btnSave.grid(row=1, column=0, pady=10)  # Placer le bouton en dessous de la zone de texte
 
         # Rendre la fenêtre responsive
         self.__responsive(frame)
@@ -52,8 +50,8 @@ class fncArreraPostite:
             self.__windows()
             with open(emplacementFile, 'r', encoding='utf-8') as file:
                 file_content = file.read()
-                self.zoneTexte.delete(1.0, END)
-                self.zoneTexte.insert(END, file_content)
+                html_content = markdown.markdown(file_content)
+                self.zoneTexte.set_html(html_content)
             self.__nameFile = emplacementFile
             return True
         else:
@@ -62,32 +60,3 @@ class fncArreraPostite:
 
     def getNamefile(self):
             return self.__nameFile
-
-    def __saveFile(self):
-        penseBete = self.zoneTexte.get(1.0,END)
-        if (self.__nameFile!=""):
-            sortie = messagebox.askyesno(self.__name+" : Postite","Voulez-vous enregistrer dans le meme fichier")
-            if (sortie == True):
-                with open(self.__nameFile, "w") as fichier:
-                    fichier.write(penseBete)
-                self.__penseBete.destroy()
-            else :
-                file = filedialog.asksaveasfilename(
-                            defaultextension='.ab',
-                            filetypes=[("Fichier Pense-bete", "*.ab")])
-                if (file):
-                    with open(file, "w") as fichier:
-                        fichier.write(penseBete)
-                    self.__penseBete.destroy()
-                else :
-                    messagebox.showerror(self.__name+" : Postite","Impossible d'enregistrer")
-        else :
-            file = filedialog.asksaveasfilename(
-                            defaultextension='.ab',
-                            filetypes=[("Fichier Pense-bete", "*.ab")])
-            if (file):
-                with open(file, "w") as fichier:
-                    fichier.write(penseBete)
-                self.__penseBete.destroy()
-            else :
-                messagebox.showerror(self.__name+" : Postite","Impossible d'enregistrer")
