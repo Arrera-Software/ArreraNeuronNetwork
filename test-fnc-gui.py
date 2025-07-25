e59dd17676119763ee31ef36066493945cea3ba3
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2025-07-25 22:59:23 +0200

    Factorisation du fichier

--- test-fnc-gui.py
+++ test-fnc-gui.py
@@ -28,100 +28,102 @@ conf = confNeuron(name="Opale",
             lienDoc="www.google.com",
             fichierLangue="language/vouvoiment/")
 
-gest = gestionnaire(conf)
-fnc = gest.getGestFNC()
+def partTask():
+    taskBoucle = True
+    while taskBoucle:
+        print("Test des fonctions de tache")
+        print(
+            "1.Ajouter une tache\n2.Lister les taches\n3.Supprimer une tache\n4.Compter le nombre de taches\n5.finir une tache\n6.Check date today\n7.Tache a faire aujourd'hui\n8.Check date towmorow\n9.Tache a faire demain\n10.Check task late\n11.Tache en retard\n0.Quitter")
+        print("__________________________\n")
+        boucleVerif = True
+        while boucleVerif:
+            nb = input("Choix : ")
+            try:
+                nb = int(nb)
+                boucleVerif = False
+            except ValueError:
+                print("Veuillez entrer un nombre valide.")
 
-while True:
-    boucleVerif = True
-    print("Teste des fonction d'Arrera Neuron NetworkW\n")
-    print("1.Taches\n0.Quitter")
-    print("__________________________\n")
+        match nb:
+            case 1:
+                title = input("Titre de la tache : ")
+                date = input("Date de la tache (YYYY-MM-DD) : ")
+                try:
+                    date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
+                    description = input("Description de la tache : ")
+                    print(fnc.getFNCTask().addTask(title, date, description))
+                except ValueError:
+                    print("Format de date invalide. Utilisez YYYY-MM-DD.")
+            case 2:
+                print(fnc.getFNCTask().getTask())
+            case 3:
+                taskId = input("Name : ")
+                sortie = fnc.getFNCTask().delTask(taskId)
+                if sortie:
+                    print("Tache supprimer")
+                else:
+                    print("Erreur")
+            case 4:
+                print(f"Nombre de taches : {fnc.getFNCTask().getNbTask()}")
+            case 5:
+                taskId = input("Name : ")
+                sortie = fnc.getFNCTask().finishTask(taskId)
+                if sortie:
+                    print("Tache terminer")
+                else:
+                    print("Erreur")
+            case 6:
+                taskId = input("Name : ")
+                print(fnc.getFNCTask().checkDateTask(taskId))
+            case 7:
+                print(fnc.getFNCTask().getNbTaskToday())
+                print(fnc.getFNCTask().getListTaskToday())
+            case 8:
+                taskId = input("Name : ")
+                print(fnc.getFNCTask().checkDateTaskTowmorow(taskId))
+            case 9:
+                print(fnc.getFNCTask().getNbTaskTowmorow())
+                print(fnc.getFNCTask().getListTaskTowmorow())
+            case 10:
+                taskId = input("Name : ")
+                print(fnc.getFNCTask().checkTaskLate(taskId))
+            case 11:
+                print(fnc.getFNCTask().getNbTaskLate())
+                print(fnc.getFNCTask().getListTaskLate())
 
+            case 0:
+                taskBoucle = False
+            case _:
+                print("Choix invalide, veuillez réessayer.")
 
-    while boucleVerif:
-        nb = input("Choix : ")
-        try :
-            nb = int(nb)
-            boucleVerif = False
-        except ValueError:
-            print("Veuillez entrer un nombre valide.")
+def main():
+    gest = gestionnaire(conf)
+    fnc = gest.getGestFNC()
 
-    match nb:
-        case 1:
-            taskBoucle = True
-            while taskBoucle :
-                print("Test des fonctions de tache")
-                print("1.Ajouter une tache\n2.Lister les taches\n3.Supprimer une tache\n4.Compter le nombre de taches\n5.finir une tache\n6.Check date today\n7.Tache a faire aujourd'hui\n8.Check date towmorow\n9.Tache a faire demain\n10.Check task late\n11.Tache en retard\n0.Quitter")
-                print("__________________________\n")
-                boucleVerif = True
-                while boucleVerif:
-                    nb = input("Choix : ")
-                    try:
-                        nb = int(nb)
-                        boucleVerif = False
-                    except ValueError:
-                        print("Veuillez entrer un nombre valide.")
-
-                match nb:
-                    case 1:
-                        title = input("Titre de la tache : ")
-                        date = input("Date de la tache (YYYY-MM-DD) : ")
-                        try:
-                            date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
-                            description = input("Description de la tache : ")
-                            print(fnc.getFNCTask().addTask(title, date, description))
-                        except ValueError:
-                            print("Format de date invalide. Utilisez YYYY-MM-DD.")
-                    case 2:
-                        print(fnc.getFNCTask().getTask())
-                    case 3:
-                        taskId = input("Name : ")
-                        sortie = fnc.getFNCTask().delTask(taskId)
-                        if sortie:
-                            print("Tache supprimer")
-                        else :
-                            print("Erreur")
-                    case 4:
-                        print(f"Nombre de taches : {fnc.getFNCTask().getNbTask()}")
-                    case 5:
-                        taskId = input("Name : ")
-                        sortie = fnc.getFNCTask().finishTask(taskId)
-                        if sortie:
-                            print("Tache terminer")
-                        else:
-                            print("Erreur")
-                    case 6:
-                        taskId = input("Name : ")
-                        print(fnc.getFNCTask().checkDateTask(taskId))
-                    case 7:
-                        print(fnc.getFNCTask().getNbTaskToday())
-                        print(fnc.getFNCTask().getListTaskToday())
-                    case 8:
-                        taskId = input("Name : ")
-                        print(fnc.getFNCTask().checkDateTaskTowmorow(taskId))
-                    case 9 :
-                        print(fnc.getFNCTask().getNbTaskTowmorow())
-                        print(fnc.getFNCTask().getListTaskTowmorow())
-                    case 10 :
-                        taskId = input("Name : ")
-                        print(fnc.getFNCTask().checkTaskLate(taskId))
-                    case 11 :
-                        print(fnc.getFNCTask().getNbTaskLate())
-                        print(fnc.getFNCTask().getListTaskLate())
-
-                    case 0:
-                        taskBoucle = False
-                    case _:
-                        print("Choix invalide, veuillez réessayer.")
-
-            break
-        case 0:
-            print("Fin du programme")
-            break
-        case _:
-            continue
+    while True:
+        boucleVerif = True
+        print("Teste des fonction d'Arrera Neuron NetworkW\n")
+        print("1.Taches\n0.Quitter")
+        print("__________________________\n")
 
 
+        while boucleVerif:
+            nb = input("Choix : ")
+            try :
+                nb = int(nb)
+                boucleVerif = False
+            except ValueError:
+                print("Veuillez entrer un nombre valide.")
 
-print(fncTask.addTask("Test de tache", datetime.date(2025,9,1),"Test de la fonction d'ajout de tache"))
-print(fncTask.getTask())
+        match nb:
+            case 1:
+                partTask()
+                break
+            case 0:
+                print("Fin du programme")
+                break
+            case _:
+                continue
+
+if __name__ == "__main__":
+    main()
