1ae62a79f9086ddf73f6cfc0b4788ea7ba1c29ce
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2025-07-26 10:12:53 +0200

    Mise a jour de la librairy

--- librairy/network.py
+++ librairy/network.py
@@ -1,6 +1,9 @@
 import requests
 class network:
     def __init__(self) :
+        self.__etatConnexion = False
+
+    def __check(self):
         try:
             _ = requests.get("https://duckduckgo.com",timeout=5)
             self.__etatConnexion = True
@@ -8,4 +11,5 @@ class network:
             self.__etatConnexion = False
     
     def getEtatInternet(self):
+        self.__check()
         return self.__etatConnexion
