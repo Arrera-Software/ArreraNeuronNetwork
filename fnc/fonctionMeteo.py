eeb233bc26997721daa92ce026e67d922bb6f1cb
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2025-07-29 18:48:18 +0200

    Ajout de getMeteoCurrentHour dans fonctionMeteo.py

--- fnc/fonctionMeteo.py
+++ fnc/fonctionMeteo.py
@@ -1,19 +1,96 @@
 from fnc.fncBase import fncBase,gestionnaire
 from datetime import datetime, timedelta
 from meteofrance_api import MeteoFranceClient
+from fnc.fonctionGPS import fncGPS
 
-class fncMete(fncBase) :
-    def __init__(self,gestionnaire:gestionnaire):
+class fncMeteo(fncBase) :
+    def __init__(self,gestionnaire:gestionnaire,gpsFnc:fncGPS):
         super().__init__(gestionnaire)
         self.__client = MeteoFranceClient()
+        self.__fncGPS = gpsFnc
+        self.__nameTown = None
+        self.__temperature = None
+        self.__humidity = None
+        self.__description = None
+        self.__date = None
+        self.__warming = None
+
+
+    def getMeteoCurrentHour(self,town:str="",latitude:str="",longitude:str=""):
+        """
+        Récupère la météo actuelle pour une ville ou des coordonnées géographiques.
+        :param town: Nom de la ville (optionnel)
+        :param latitude: Latitude (optionnel)
+        :param longitude: Longitude (optionnel)
+        :return: True si les données sont récupérées avec succès, False sinon.
+        """
+        if self._gestionnaire.getNetworkObjet().getEtatInternet():
+            if town:
+                try:
+                    townWeather = self.__client.search_places(town)[0]
+                    weather = self.__client.get_forecast_for_place(townWeather)
+                    dictMeteo = weather.current_forecast
+                    self.__nameTown = townWeather.name
+                    self.__temperature = dictMeteo['T']['value']
+                    self.__humidity = dictMeteo["humidity"]
+                    self.__description = dictMeteo['weather']['desc']
+                    return True
+                except Exception as e:
+                    # print(f"Erreur lors de la récupération des données météo : {e}")
+                    return False
+            elif latitude and longitude:
+                try:
+                    town = self.__fncGPS.getTownWithLatitudeAndLongitude(latitude,longitude)
+                    if town is None:
+                        return False
+                    else:
+                        townWeather = self.__client.search_places(town)[0]
+                        weather = self.__client.get_forecast_for_place(townWeather)
+                        dictMeteo = weather.current_forecast
+                        self.__nameTown = townWeather.name
+                        self.__temperature = dictMeteo['T']['value']
+                        self.__humidity = dictMeteo["humidity"]
+                        self.__description = dictMeteo['weather']['desc']
+                        return True
+                except Exception as e:
+                    # print(f"Erreur lors de la récupération des données météo : {e}")
+                    return False
+            else :
+                if self.__fncGPS.locate():
+                    try :
+                        townWeather = self.__client.search_places(self.__fncGPS.getTown())[0]
+                        weather = self.__client.get_forecast_for_place(townWeather)
+                        dictMeteo = weather.current_forecast
+                        self.__nameTown = townWeather.name
+                        self.__temperature = dictMeteo['T']['value']
+                        self.__humidity = dictMeteo["humidity"]
+                        self.__description = dictMeteo['weather']['desc']
+                        return True
+                    except Exception as e:
+                        # print(f"Erreur lors de la récupération des données météo : {e}")
+                        return False
+                else :
+                    return False
+        else :
+            return False
+
 
     
     # def getDataMeteoNow(self,latitude:str,longitude:str):
     # def getDateMetoTowmorowMorning(self,latitude:str,longitude:str):
     # def getDateMetoTowmorowNoon(self,latitude:str,longitude:str):
-    # def gettemperature(self):#permet de recuperé la temperature
-    # def gethumiditer(self):#permet de recuperé le taux d'humiditer en %
-    # def getdescription(self):#permet de recuperé la description de temp en fr
+    def getNameTown(self):
+        return self.__nameTown
+
+    def getTemperature(self):
+        return self.__temperature
+
+    def getHumidity(self):#permet de recuperé le taux d'humiditer en %
+        return self.__humidity
+
+    def getDescription(self):
+        return self.__description
+
     # def geticon(self):
     """
     Ville : {'dt': 1753732800, 
