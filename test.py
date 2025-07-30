395df5a8d2132798920e3b5d3f6bfbc577715366
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2025-07-30 14:33:07 +0200

    Test de la partie alert

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+"""
 from meteofrance_api import MeteoFranceClient
 
 # Initialiser le client
@@ -12,3 +13,24 @@ weather = client.get_forecast_for_place(paris)
 
 # Afficher quelques informations
 print("Ville :",weather.current_forecast)
+"""
+
+from meteofrance_api import MeteoFranceClient
+
+# Initialiser le client
+client = MeteoFranceClient()
+dictWarning = client.get_warning_dictionary("fr")
+
+# Spécifier le lieu (par exemple, latitude et longitude pour Paris)
+latitude = 48.8566
+longitude = 2.3522
+
+# Obtenir les alertes pour le lieu spécifié
+alertes = client.get_warning_current_phenomenons("62").phenomenons_max_colors
+
+for alerte in alertes:
+    print(dictWarning.get_phenomenon_by_id(int(alerte['phenomenon_id']))['name']+
+          " : "+ dictWarning.get_color_by_id(alerte['phenomenon_max_color_id'])['name'])
+
+# Afficher les alertes
+print(alertes)
