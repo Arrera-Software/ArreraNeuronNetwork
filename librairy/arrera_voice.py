5e96f4f28aac6525e27c8d5e9ed954f0f5852836
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2025-07-31 13:57:11 +0200

    Ajout de la lib arrera_voice.py

---
+++ librairy/arrera_voice.py
@@ -0,0 +1,83 @@
+from playsound3 import playsound as pl
+from pyttslib import TextToSpeech
+from librairy.network import *
+from librairy.travailJSON import jsonWork
+import speech_recognition as sr
+
+
+class CArreraVoice:
+    def __init__(self,configFile:jsonWork):
+        self.__configFile = configFile
+        self.__emplacementSoundMicro = self.__configFile.lectureJSON("fileMicro")
+        self.__soundMicro = True
+        self.__listWord = []
+        self.__nbWord = 0
+        self.__outPutText = ""
+        self.loadConfig()
+
+        if network().getEtatInternet() == True:
+            self.__tts = TextToSpeech(engine="google")
+            self.__tts.set_voice("fr")
+        else:
+            self.__tts = TextToSpeech(engine="pyttsx3",engine_config={
+                "rate": 150,    # Words per minute
+                "volume": 0.8,  # Volume level (0.0 to 1.0)
+            })
+            self.__tts.set_voice("French (France)")
+
+
+
+    def loadConfig(self):
+        if (self.__configFile.lectureJSON("soundMicro") == "1" ):
+            self.__soundMicro = True
+        else:
+            self.__soundMicro = False
+        self.__listWord = self.__configFile.lectureJSONList("listWord")
+        self.__nbWord = len(self.__listWord)
+
+    def say(self,text:str):
+        self.__tts.speak(text)
+
+    def playFile(self,file:str):
+        pl(file)
+
+    def listen(self):
+        if self.__soundMicro:
+            pl(self.__emplacementSoundMicro)
+
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            r.adjust_for_ambient_noise(source)
+            audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio, language='fr-FR')
+            self.__outPutText = text
+            return 0
+        except sr.UnknownValueError:
+            return -1
+        except sr.RequestError as e:
+            return -2
+
+    def getTextMicro(self):
+        return self.__outPutText
+
+    def getNbWord(self):
+        return self.__nbWord
+
+    def trigerWord(self):
+        if self.__nbWord == 0:
+            return -3
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            r.adjust_for_ambient_noise(source)
+            audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio, language='fr-FR')
+            for word in self.__listWord:
+                if word in text:
+                    return 1
+        except sr.UnknownValueError:
+            return -1
+        except sr.RequestError as e:
+            return -2
+        return 0
