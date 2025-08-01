b40aeffe6250e1f957f42aa584ab007db4fe3cd3
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2025-08-01 13:23:29 +0200

    Debut de reecriture de la fncRadio

--- fnc/fonctionRadio.py
+++ fnc/fonctionRadio.py
@@ -1,13 +1,47 @@
-import webbrowser
-from librairy.network import*
+from fnc.fncBase import fncBase,gestionnaire
+from pyradios import RadioBrowser
+import threading as th
+import vlc
 
-class fncRadio :
-    def __init__(self,network:network):
-        self.__etatNetwork = network.getEtatInternet()
+class fncRadio(fncBase) :
+    def __init__(self, gestionnaire:gestionnaire):
+        super().__init__(gestionnaire)
+        self.__rdBrowser = RadioBrowser()
+        self.__etatNetwork = self._gestionnaire.getNetworkObjet().getEtatInternet()
+        self.__stations = []
+        self.__player = None
+        self.__isRunning = False
+        self.__thread = None
+
+    def __searchRadio(self, radio):
+        self.__stations = []
+        self.__stations = self.__rdBrowser.search(name=radio, country="France")
+        return bool(self.__stations)
+
+    def __launchRadioThread(self):
+        if not self.__isRunning and self.__stations and self.__etatNetwork:
+            url = self.__stations[0]['url']
+            self.__player = vlc.MediaPlayer(url)
+            self.__isRunning = True
+            self.__player.play()
+
+    def __launchRadio(self):
+        if not self.__isRunning:
+            self.__thread = th.Thread(target=self.__launchRadioThread)
+            self.__thread.start()
+
+    def stopRadio(self):
+        if self.__player and self.__isRunning:
+            self.__player.stop()
+            self.__player = None
+            self.__isRunning = False
+            if self.__thread:
+                self.__thread.join(timeout=1)
+
     
     def startEurope1(self):
-        if (self.__etatNetwork) :
-            webbrowser.open("https://www.ecouter-en-direct.com/europe-1/")
+        if self.__etatNetwork:
+
             return True
         else :
             return False
