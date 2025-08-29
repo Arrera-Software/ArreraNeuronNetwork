1f8806ac0392f80ffd5ac5de5caf1eb1b51acb01
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2025-08-29 12:58:42 +0200

    Placement dans le frameLogoTitle

--- gui/GUIAgenda.py
+++ gui/GUIAgenda.py
@@ -20,17 +20,39 @@ class GUIAgenda(GuiBase):
         frameCalendar = self._arrtk.createFrame(self.__frameMain,bg="green")
 
         # Configuration des frames
-        self.__frameMain.columnconfigure(0, weight=0)  # colonne gauche: taille fixe (pas d'étirement)
-        self.__frameMain.columnconfigure(1, weight=1)  # colonne droite: prend tout l'espace restant
+        self.__frameMain.columnconfigure(0, weight=0)
+        self.__frameMain.columnconfigure(1, weight=1)
         self.__frameMain.rowconfigure(0, weight=1)
         self.__frameMain.rowconfigure(1, weight=1)
         self.__frameMain.rowconfigure(2, weight=1)
+
+        frameLogoTitle.grid_columnconfigure(0, weight=0)  # colonne gauche (label)
+        frameLogoTitle.grid_columnconfigure(1, weight=1)  # espace flexible
+        frameLogoTitle.grid_columnconfigure(2, weight=0)  # colonne droite (label)
+
+        frameLogoTitle.grid_rowconfigure(0, weight=1)  # espace haut
+        frameLogoTitle.grid_rowconfigure(1, weight=0)  # rangée centrale (labels)
+        frameLogoTitle.grid_rowconfigure(2, weight=1)  # espace bas
+
+        # Widget
+        lLogoApp = self._arrtk.createLabel(frameLogoTitle,
+                                           text="Logo",bg="red") # A remplacer par le logo de l'application
+        lTitleApp = self._arrtk.createLabel(frameLogoTitle,
+                                            text=self._gestionnaire.getName()+" : Agenda",
+                                            ppolice="Arial", ptaille=20, pstyle="bold")
+
+
 
         # Placement des frames
         frameEventDay.grid(row=0, column=1, rowspan=3, sticky="nsew", padx=0, pady=0)
         frameLogoTitle.grid(row=0, column=0, sticky="nw", padx=0, pady=0)
         frameBTN.grid(row=1, column=0, sticky="w", padx=0, pady=0)
         frameCalendar.grid(row=2, column=0, sticky="sw", padx=0, pady=0)
+
+        # Placement des widgets
+        # Logo et titre
+        lLogoApp.grid(row=1, column=0, sticky="w")
+        lTitleApp.grid(row=1, column=2, sticky="e")
 
         # Affichage principal
         self.__frameMain.grid(row=0, column=0, sticky="nsew")
