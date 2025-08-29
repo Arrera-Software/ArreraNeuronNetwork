29ed3f07222fb05bcafb25961c93082cb99672d2
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2025-08-29 13:07:00 +0200

    Placement dans le frameBTN

--- gui/GUIAgenda.py
+++ gui/GUIAgenda.py
@@ -34,12 +34,27 @@ class GUIAgenda(GuiBase):
         frameLogoTitle.grid_rowconfigure(1, weight=0)  # rangée centrale (labels)
         frameLogoTitle.grid_rowconfigure(2, weight=1)  # espace bas
 
+        frameBTN.grid_columnconfigure(0, weight=1)
+        frameBTN.grid_columnconfigure(1, weight=0)
+        frameBTN.grid_columnconfigure(2, weight=1)
+
+        frameBTN.grid_rowconfigure(0, weight=0)
+        frameBTN.grid_rowconfigure(1, weight=1, minsize=24)  # écart vertical entre les deux boutons
+        frameBTN.grid_rowconfigure(2, weight=0)
+
         # Widget
         lLogoApp = self._arrtk.createLabel(frameLogoTitle,
                                            text="Logo",bg="red") # A remplacer par le logo de l'application
         lTitleApp = self._arrtk.createLabel(frameLogoTitle,
                                             text=self._gestionnaire.getName()+" : Agenda",
                                             ppolice="Arial", ptaille=20, pstyle="bold")
+
+        btnCreateEvent = self._arrtk.createButton(frameBTN,text="Créer\nun événement",
+                                                  ppolice="Arial",ptaille=15,pstyle="bold",
+                                                  bg=self._btnColor,fg=self._btnTexteColor)
+        btnSupprimerEvent = self._arrtk.createButton(frameBTN, text="Supprimer\nun événement",
+                                                  ppolice="Arial", ptaille=15, pstyle="bold",
+                                                  bg=self._btnColor, fg=self._btnTexteColor)
 
 
 
@@ -53,6 +68,9 @@ class GUIAgenda(GuiBase):
         # Logo et titre
         lLogoApp.grid(row=1, column=0, sticky="w")
         lTitleApp.grid(row=1, column=2, sticky="e")
+        # Boutons
+        btnCreateEvent.grid(row=0, column=1, sticky="n")
+        btnSupprimerEvent.grid(row=2, column=1, sticky="s")
 
         # Affichage principal
         self.__frameMain.grid(row=0, column=0, sticky="nsew")
