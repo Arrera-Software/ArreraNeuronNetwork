6063fe5455c5e5d2583b1a42420bbd191cc6bf2d
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2025-08-31 20:32:34 +0200

    Creation de l'interface d'ajout d'event

--- gui/GUIAgenda.py
+++ gui/GUIAgenda.py
@@ -25,7 +25,7 @@ class GUIAgenda(GuiBase):
         self._screen.columnconfigure(0, weight=1)
         # Creation des frames Maitre
         self.__frameMain = self._arrtk.createFrame(self._screen)
-        self.__frameAddEvent = self._arrtk.createFrame(self.__frameMain)
+        self.__frameAddEvent = self._arrtk.createFrame(self._screen)
         self.__frameConfirm = self._arrtk.createFrame(self.__frameMain)
         # Frame fille
         frameLogoTitle = self._arrtk.createFrame(self.__frameMain)
@@ -33,7 +33,9 @@ class GUIAgenda(GuiBase):
         frameEventDay = self._arrtk.createFrame(self.__frameMain,wightBoder=2)
         frameCalendar = self._arrtk.createFrame(self.__frameMain)
 
-        frameHeure = self._arrtk.createFrame(self.__frameAddEvent)
+        self.__frameAdd = self._arrtk.createFrame(self.__frameAddEvent)
+        frameBTNFAdd = self._arrtk.createFrame(self.__frameAdd)
+        self.__frameHeure = self._arrtk.createFrame(self.__frameAddEvent)
 
         # Configuration des frames
         # Frame Main
@@ -63,6 +65,24 @@ class GUIAgenda(GuiBase):
         frameEventDay.grid_rowconfigure(1, weight=0)
         frameEventDay.grid_rowconfigure(2, weight=1)
 
+        self.__frameAddEvent.rowconfigure(0, weight=1)
+        self.__frameAddEvent.columnconfigure(0, weight=1)
+
+        self.__frameAdd.grid_columnconfigure(0, weight=1)
+        self.__frameAdd.grid_rowconfigure(0, weight=0)
+        self.__frameAdd.grid_rowconfigure(1, weight=0)
+        self.__frameAdd.grid_rowconfigure(2, weight=0)
+        self.__frameAdd.grid_rowconfigure(3, weight=0)
+        self.__frameAdd.grid_rowconfigure(4, weight=0)
+        self.__frameAdd.grid_rowconfigure(5, weight=0)
+        self.__frameAdd.grid_rowconfigure(6, weight=1)
+        self.__frameAdd.grid_rowconfigure(7, weight=0)
+
+        frameBTNFAdd.grid_columnconfigure(0, weight=0)
+        frameBTNFAdd.grid_columnconfigure(1, weight=1)
+        frameBTNFAdd.grid_columnconfigure(2, weight=0)
+        frameBTNFAdd.grid_rowconfigure(0, weight=0)
+
         # Frame Add Event
 
 
@@ -82,7 +102,8 @@ class GUIAgenda(GuiBase):
         # Boutons
         btnCreateEvent = self._arrtk.createButton(frameBTN,text="Créer\nun événement",
                                                   ppolice="Arial",ptaille=15,pstyle="bold",
-                                                  bg=self._btnColor,fg=self._btnTexteColor)
+                                                  bg=self._btnColor,fg=self._btnTexteColor,
+                                                  command=lambda : self.__viewAddEvent(0))
         btnSupprimerEvent = self._arrtk.createButton(frameBTN, text="Supprimer\nun événement",
                                                   ppolice="Arial", ptaille=15, pstyle="bold",
                                                   bg=self._btnColor, fg=self._btnTexteColor)
@@ -97,18 +118,30 @@ class GUIAgenda(GuiBase):
         self.__labelEvent = self._arrtk.createLabel(frameEventDay,text="EVENT",ppolice="Arial", ptaille=20, pstyle="bold")
         self.__btnAddEventDay = self._arrtk.createButton(frameEventDay,text="Ajouter un événement",
                                                     ppolice="Arial", ptaille=25,bg=self._btnColor,
-                                                         fg=self._btnTexteColor)
+                                                         fg=self._btnTexteColor,command=lambda : self.__viewAddEvent(1))
 
 
         # Frame Add Event
 
-        self.__labelTitleAddEvent = self._arrtk.createLabel(self.__frameAddEvent,
+        self.__labelTitleAddEvent = self._arrtk.createLabel(self.__frameAdd,
                                                             text="Ajouter un événement",
                                                             ppolice="Arial", ptaille=30, pstyle="bold")
-        self.__entryNameEvent = self._arrtk.createEntry(self.__frameAddEvent,pppolice="Arial", pptaille=20)
-        self.__entryDescriptionEvent = self._arrtk.createEntry(self.__frameAddEvent,pppolice="Arial", pptaille=20)
-        self.__entryLieuEvent = self._arrtk.createEntry(self.__frameAddEvent,pppolice="Arial", pptaille=20)
+        self.__calendarAddEvent = Calendar(self.__frameAdd,selectmode="day",year=date.today().year,
+                                           month=date.today().month,locale="fr_FR",firstweekday="monday",
+                                           showweeknumbers=False,borderwidth=0)
+        self.__labelDateSelected = self._arrtk.createLabel(self.__frameAdd,ppolice="Arial", ptaille=15)
+        wEntryName,self.__entryNameEvent = self._arrtk.createEntryLegend(self.__frameAdd,text="Titre : ",ppolice="Arial", ptaille=20,gridUsed=True)
+        wEntryDescription,self.__entryDescriptionEvent = self._arrtk.createEntryLegend(self.__frameAdd,ppolice="Arial", ptaille=20,text="Description : ",gridUsed=True)
+        wEntryLieu,self.__entryLieuEvent = self._arrtk.createEntryLegend(self.__frameAdd,ppolice="Arial", ptaille=20,text="Lieu :",gridUsed=True)
+        self.__checkHour = self._arrtk.createCheckbox(self.__frameAdd,text="Définir une heure",bg=self._btnColor)
 
+        btnAddEvent = self._arrtk.createButton(frameBTNFAdd,text="Ajouter",
+                                              ppolice="Arial", ptaille=20,
+                                              bg=self._btnColor, fg=self._btnTexteColor)
+
+        btnCancelEvent = self._arrtk.createButton(frameBTNFAdd,text="Annuler",
+                                               ppolice="Arial", ptaille=20,command=self.__backToMain,
+                                               bg=self._btnColor, fg=self._btnTexteColor)
 
         # Placement des frames
         frameEventDay.grid(row=0, column=1, rowspan=3, sticky="nsew", padx=0, pady=0)
@@ -129,6 +162,17 @@ class GUIAgenda(GuiBase):
         self.__labelDate.grid(row=0, column=0, sticky="nw", padx=10, pady=(10, 5))
         self.__labelEvent.grid(row=1, column=0, sticky="w",  padx=10, pady=(0, 10))
         self.__btnAddEventDay.grid(row=2, column=0, sticky="s", padx=10, pady=10)
+
+        # Ajout Event
+        self.__labelTitleAddEvent.grid(row=0, column=0, sticky="ew", padx=12, pady=(12, 6))
+        wEntryName.grid(row=2, column=0, sticky="ew", padx=12, pady=6)
+        wEntryDescription.grid(row=3, column=0, sticky="ew", padx=12, pady=6)
+        wEntryLieu.grid(row=4, column=0, sticky="ew", padx=12, pady=6)
+        self.__checkHour.grid(row=5, column=0, sticky="ew", padx=12, pady=6)
+        btnCancelEvent.grid(row=0, column=0, sticky="w", padx=(10, 6), pady=8)
+        btnAddEvent.grid(   row=0, column=2, sticky="e", padx=(6, 10), pady=8)
+        frameBTNFAdd.grid(row=7, column=0, sticky="sew", padx=12, pady=12)
+
         # Affichage principal
         self.__frameMain.grid(row=0, column=0, sticky="nsew")
 
@@ -137,6 +181,12 @@ class GUIAgenda(GuiBase):
         
         # Check du mini calendar 
         self.__miniCalendar.bind("<<CalendarSelected>>", self.__dateSelectedOnCalendar)
+        self._screen.update()
+
+    def __backToMain(self):
+        self.__frameAddEvent.grid_forget()
+        self.__frameMain.grid(row=0, column=0, sticky="nsew")
+        self._screen.update()
 
     def __viewEventDay(self, date):
         """date (YYYY-MM-DD)"""
@@ -152,6 +202,7 @@ class GUIAgenda(GuiBase):
                     texte += "- " + event + "\n"
                 self.__labelEvent.configure(text=texte)
             self.__dateSelected = date
+            self._screen.update()
             return True
         except Exception as e:
             return False
@@ -159,4 +210,21 @@ class GUIAgenda(GuiBase):
     def __dateSelectedOnCalendar(self,event):
         date = self.__miniCalendar.get_date()
         self.__viewEventDay(datetime.strptime(date, "%d/%m/%Y").strftime("%Y-%m-%d"))
+
+    def __viewAddEvent(self,mode:int):
+
+        self.__calendarAddEvent.grid_forget()
+        self.__labelDateSelected.grid_forget()
+
+        match mode :
+            case 0:
+                self.__calendarAddEvent.grid(row=1, column=0, sticky="n",  padx=12, pady=6)
+            case 1 :
+                self.__labelDateSelected.configure(text="Date sélectionnée : "+self.__dateSelected)
+                self.__labelDateSelected.grid(row=1, column=0, sticky="n",  padx=12, pady=6)
+
+        self.__frameMain.grid_forget()
+        self.__frameAddEvent.grid(row=0, column=0, sticky="nsew")
+        self.__frameAdd.grid(row=0, column=0, sticky="nsew")
+        self._screen.update()
         
