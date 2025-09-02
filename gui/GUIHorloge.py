2bb895e91ac31b11adf692d001515248e0568028
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2025-09-02 15:03:33 +0200

    Ajout de la partie fonctionnel du minuteur

--- gui/GUIHorloge.py
+++ gui/GUIHorloge.py
@@ -1,3 +1,5 @@
+from tkinter import StringVar
+
 from gui.guibase import GuiBase,gestionnaire
 
 class GUIHorloge(GuiBase):
@@ -5,8 +7,12 @@ class GUIHorloge(GuiBase):
         super().__init__(gest, "Horloge")
         self.__fncHorloge = self._gestionnaire.getGestFNC().getFNCHorloge()
         self.__clockEnable = False
+        self.__minuteurEnable = False
 
     def _mainframe(self):
+        # Variables
+        self.__varMinute = StringVar(self._screen)
+        self.__varSec = StringVar(self._screen)
         # Conf de la fenetre
         self._screen.grid_rowconfigure(0, weight=0)
         self._screen.grid_rowconfigure(1, weight=1)
@@ -16,6 +22,10 @@ class GUIHorloge(GuiBase):
         self.__frameHorloge = self._arrtk.createFrame(self._screen)
         self.__frameChrono = self._arrtk.createFrame(self._screen,bg="red")
         self.__frameMinuteur = self._arrtk.createFrame(self._screen,bg="yellow")
+
+        # Frame secondaire
+        self.__frameSetMinuteur = self._arrtk.createFrame(self.__frameMinuteur)
+        self.__frameViewMinuteur = self._arrtk.createFrame(self.__frameMinuteur)
 
         # Conf frame
         self.__frameNav.grid_columnconfigure(0, weight=1)  # espace à gauche
@@ -28,6 +38,23 @@ class GUIHorloge(GuiBase):
         self.__frameHorloge.grid_rowconfigure(0, weight=1)
         self.__frameHorloge.grid_columnconfigure(0, weight=1)
 
+        self.__frameMinuteur.grid_rowconfigure(0, weight=1)
+        self.__frameMinuteur.grid_columnconfigure(0, weight=1)
+
+        self.__frameSetMinuteur.grid_rowconfigure(0, weight=1)
+        self.__frameSetMinuteur.grid_rowconfigure(1, weight=1)
+        self.__frameSetMinuteur.grid_rowconfigure(2, weight=1)
+        self.__frameSetMinuteur.grid_columnconfigure(0, weight=1, uniform="cols")
+        self.__frameSetMinuteur.grid_columnconfigure(1, weight=1, uniform="cols")
+        self.__frameSetMinuteur.grid_columnconfigure(2, weight=1, uniform="cols")
+
+        self.__frameViewMinuteur.grid_columnconfigure(0, weight=1)
+
+        self.__frameViewMinuteur.grid_rowconfigure(0, weight=1)
+        self.__frameViewMinuteur.grid_rowconfigure(1, weight=0)
+        self.__frameViewMinuteur.grid_rowconfigure(2, weight=0)
+        self.__frameViewMinuteur.grid_rowconfigure(3, weight=1)
+
         # Widget
         # Nav
         btnHorloge = self._arrtk.createButton(self.__frameNav,text="Horloge",command=self.__viewHorloge)
@@ -37,6 +64,26 @@ class GUIHorloge(GuiBase):
         self.__labelViewClock = self._arrtk.createLabel(self.__frameHorloge,text="00:00:00",
                                                         ppolice="Arial",ptaille=60,pstyle="bold")
 
+        # Minuteur
+        picketMinuteur = self._arrtk.createHourPickert(self.__frameSetMinuteur,
+                                                       varHour=self.__varMinute,
+                                                       varMinute=self.__varSec)
+        btnReset = self._arrtk.createButton(self.__frameSetMinuteur,text="Reset"
+                                            ,command=self.__resetMinuteur)
+        btnStartMinuteur1 = self._arrtk.createButton(self.__frameSetMinuteur,text="Start",
+                                                     command=self.__startMinuteur)
+        btnStartMinuteur2 = self._arrtk.createButton(self.__frameSetMinuteur,text="1 Min",
+                                                     command=self.__setOneMin)
+        btnStartMinuteur3 = self._arrtk.createButton(self.__frameSetMinuteur, text="3 Min",
+                                                     command=self.__setThreeMin)
+
+
+        # View Minuteur
+        self.__labelViewTimeMinuteur = self._arrtk.createLabel(self.__frameViewMinuteur,text="00:00:00",
+                                                        ppolice="Arial",ptaille=60,pstyle="bold")
+        btnStopMinuteur = self._arrtk.createButton(self.__frameViewMinuteur,text="Stop",
+                                                  command=self.__fncHorloge.stopMinuteur)
+
         # Placement des widget
         btnHorloge.grid( row=0, column=1, padx=(8, 8), pady=10)
         btnMinuteur.grid(row=0, column=2, padx=(8, 8), pady=10)
@@ -44,8 +91,17 @@ class GUIHorloge(GuiBase):
 
         self.__labelViewClock.grid(row=0, column=0, padx=10, pady=10)
 
+        picketMinuteur.grid(row=1, column=1, padx=12, pady=12)
+        btnStartMinuteur2.grid(row=1, column=0, padx=8, pady=8)
+        btnStartMinuteur3.grid(row=1, column=2, padx=8, pady=8)
+        btnReset.grid(row=2, column=1, padx=8, pady=(6, 2))
+        btnStartMinuteur1.grid(row=3, column=1, padx=8, pady=(2, 12))
+
         self.__frameNav.grid(row=0, column=0, sticky="ew")
         self.__frameHorloge.grid(row=1, column=0, sticky="nsew")
+
+        self.__labelViewTimeMinuteur.grid(row=1, column=0, padx=12, pady=12)
+        btnStopMinuteur.grid(row=2, column=0, padx=12, pady=(8, 16))
 
         self.__clockEnable = True
         self.__activeClock()
@@ -65,6 +121,12 @@ class GUIHorloge(GuiBase):
         self.__disableAllFrame()
         self.__clockEnable = False
         self.__frameMinuteur.grid(row=1, column=0, sticky="nsew")
+        if self.__minuteurEnable:
+            self.__frameSetMinuteur.grid_forget()
+            self.__frameViewMinuteur.grid(row=0, column=0, sticky="nsew")
+        else :
+            self.__frameViewMinuteur.grid_forget()
+            self.__frameSetMinuteur.grid(row=0, column=0, sticky="nsew")
 
     def activeMinuteur(self):
         self.active()
@@ -79,9 +141,65 @@ class GUIHorloge(GuiBase):
         self.active()
         self.__viewChrono()
 
+    # Horloge
     def __activeClock(self):
         if self.__clockEnable:
             current_time = self.__fncHorloge.getHorloge()
             self.__labelViewClock.configure(text=current_time)
             self._screen.after(1000, self.__activeClock)
 
+    # Minuteur
+    def __setThreeMin(self):
+        self.__varMinute.set("03")
+        self.__varSec.set("00")
+
+    def __setOneMin(self):
+        self.__varMinute.set("01")
+        self.__varSec.set("00")
+
+    def __resetMinuteur(self):
+        self.__varMinute.set("00")
+        self.__varSec.set("00")
+        self.__labelViewTimeMinuteur.configure(text="00:00:00")
+
+    def __startMinuteur(self):
+        min = self.__varMinute.get()
+        sec = self.__varSec.get()
+        if min == "" and sec == "":
+            return False
+        try :
+            min = int(min)
+            sec = int(sec)
+
+            time = min * 60 + sec
+            if time <= 0:
+                return False
+            else :
+                self.__fncHorloge.startMinuteur(time)
+                self.__minuteurEnable = True
+                self.__frameViewMinuteur.grid(row=0, column=0, sticky="nsew")
+                self.__frameSetMinuteur.grid_forget()
+                self._screen.after(1, self.__updateMinuteur)
+                return True
+        except ValueError:
+            return False
+
+    def __updateMinuteur(self):
+        self.__minuteurEnable = self.__fncHorloge.getStatMinuteur()
+        if self.__minuteurEnable:
+            time = self.__fncHorloge.getTimeMinuteur()
+            if time is None:
+                self.__minuteurEnable = False
+                self.__labelViewTimeMinuteur.configure(text="00:00:00")
+                self.__frameViewMinuteur.grid_forget()
+                self.__frameSetMinuteur.grid(row=0, column=0, sticky="nsew")
+                return False
+            else :
+                self.__labelViewTimeMinuteur.configure(text=time)
+                self._screen.after(1, self.__updateMinuteur)
+                return True
+        else:
+            self.__minuteurEnable = False
+            self.__frameViewMinuteur.grid_forget()
+            self.__frameSetMinuteur.grid(row=0, column=0, sticky="nsew")
+            return False
