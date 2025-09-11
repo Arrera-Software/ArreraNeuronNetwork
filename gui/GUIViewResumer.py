f77528180400f309486e2f571d13e813c3ac871d
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2025-09-11 12:57:14 +0200

    Ajout de l'affichage du top

--- gui/GUIViewResumer.py
+++ gui/GUIViewResumer.py
@@ -15,14 +15,29 @@ class GUIViewResumer(GuiBase):
         self._screen.minsize(500, 620)
         self._screen.maxsize(500, 620)
 
-        self._screen.grid_columnconfigure(0, weight=1)     # 1 seule colonne, qui s’étire
-        self._screen.grid_rowconfigure(0, weight=0, minsize=25)  # rangée du haut: petite hauteur (50 px)
+        self._screen.grid_columnconfigure(0, weight=1)
+        self._screen.grid_rowconfigure(0, weight=0, minsize=25)
         self._screen.grid_rowconfigure(1, weight=1)
+        self._screen.grid_rowconfigure(2, weight=0, minsize=25)
 
         # Frame
         topFrame = self._arrtk.createFrame(self._screen,bg="red")
         labelFrame = self._arrtk.createFrame(self._screen,bg="blue")
+        btnFrame = self._arrtk.createFrame(self._screen,bg="green")
+
+        # IMG
+        imgLogo = self._arrtk.createImage(pathDark=self._gestionnaire.getConfigFile().icon,
+                                          pathLight=self._gestionnaire.getConfigFile().icon,
+                                          tailleX=50,tailleY=50)
+        # Widgets
+        logoLabel = self._arrtk.createLabel(topFrame,image=imgLogo)
+        self.__titleLabel = self._arrtk.createLabel(topFrame,text="Resumer",ppolice="Arial",
+                                                    ptaille=35,pstyle="bold")
 
         # Affichage
-        topFrame.grid(row=0, column=0, sticky="nsew")    # pleine largeur, faible hauteur
+        logoLabel.pack(side="left", anchor="center", padx=(6, 10), pady=6)
+        self.__titleLabel.pack(side="left", anchor="center", pady=6)
+
+        topFrame.grid(row=0, column=0, sticky="nsew")
         labelFrame.grid(row=1, column=0, sticky="nsew")
+        btnFrame.grid(row=2, column=0, sticky="nsew")
