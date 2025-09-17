2cd3acdc90db8c9d689a1fe7d03b3ce5b6527a12
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2025-09-17 09:28:37 +0200

    Reecriture de la partie time

--- neuron/time.py
+++ neuron/time.py
@@ -1,54 +1,55 @@
 from neuron.CNeuronBase import *
 
 class neuroneTime(neuronBase):
+    def __init__(self,gestionnaire:gestionnaire):
+        super().__init__(gestionnaire)
+        self.__libTime = self._gestionnaire.getArrDate()
 
     def neurone(self,requette:str):
         #Initilisation des variable nbRand et text et valeur
         self._listSortie = ["",""]
-        self._valeurOut = 0
-        if self._gestNeuron.getTime():
-            #reponse neuron time
-            if ((("resumer" in requette) and ("tache" in requette)) or
-                    (("resumer" in requette) and ("agenda" in requette))):
-                    nb,listout = self._fonctionArreraNetwork.sortieResumerTacheAgenda()
-                    self._listSortie = listout
-                    self._valeurOut = nb
-                    self._objHistorique.setAction("Resumer des tache et des evenement du jour")
-            elif self.__neuronTime(requette) == 1:
-                if self._valeurOut==0:
-                    self._valeurOut = self._gestionnaire.verrifSortie(self._listSortie[0])
-            elif self.__neuronAgenda(requette) == 1:
-                if self._valeurOut==0:
-                    self._valeurOut = self._gestionnaire.verrifSortie(self._listSortie[0])
-            elif self.neuronTache(requette) == 1:
-                if self._valeurOut==0:
-                    self._valeurOut = self._gestionnaire.verrifSortie(self._listSortie[0])
+        self._valeurOut = self.__neuronTime(requette)
+        if self._valeurOut == 0:
+            self._valeurOut = self.__neuronAgenda(requette)
+            if self._valeurOut == 0:
+                self._valeurOut = self.__neuronTache(requette)
+
 
     def __neuronTime(self,requette:str):
-        if "heure" in requette:
-            self._listSortie = [self._fonctionArreraNetwork.sortieHeure(), ""]
-            self._valeurOut = 1
+        if self._keyword.checkTime(requette,"hour"):
+            hour = self.__libTime.heure()
+            minute = self.__libTime.minute()
+            self._listSortie =  [self._language.getPhraseHeure(hour,minute),""]
             return 1
-        elif "date" in requette:
-            self._listSortie = [self._fonctionArreraNetwork.sortieDate(), ""]
-            self._valeurOut = 1
+        elif self._keyword.checkTime(requette,"date"):
+            day = self.__libTime.jourSimple()
+            month = self.__libTime.mois()
+            year = self.__libTime.annes()
+            self._listSortie = [self._language.getPhraseDate(jour=day,
+                                                             mois=month,
+                                                             annee=year),""]
             return 1
-        elif "chronometre" in requette or "chrono" in requette:
-            self._listSortie = [self._fonctionArreraNetwork.sortieOpenChrono(), ""]
-            self._valeurOut = 5
+        elif self._keyword.checkTime(requette,"chrono"):
+            self._gestGUI.activeChrono()
+            self._listSortie = [self._language.getPhraseTime("1"),""]
             return 1
-        elif "horloge" in requette:
-            self._listSortie = [self._fonctionArreraNetwork.sortieOpenHorloge(), ""]
-            self._valeurOut = 5
+        elif self._keyword.checkTime(requette,"clock"):
+            self._gestGUI.activeHorloge()
+            self._listSortie = [self._language.getPhraseTime("2"),""]
             return 1
-        elif "minuteur" in requette:
-            self._listSortie = [self._fonctionArreraNetwork.sortieOpenSimpleMinuteur(), ""]
-            self._valeurOut = 5
+        elif self._keyword.checkTime(requette,"clock"):
+            self._gestGUI.activeHorloge()
+            self._listSortie = [self._language.getPhraseTime("3"),""]
+            return 1
+        elif self._keyword.checkTime(requette,"timer"):
+            self._gestGUI.activeMinuteur()
+            self._listSortie = [self._language.getPhraseTime("4"),""]
             return 1
         else :
             return 0
 
-    def __neuronAgenda(self,requette:str): # Rendez-vous
+    def __neuronAgenda(self,requette:str):
+        """
         if "evenement" in requette or "agenda" in requette or "rendez vous" in requette or "rappel" in requette:
             if "ajoute" in requette or "ajouter" in requette or "add" in requette or "ajout" in requette:
                 self._listSortie = [self._fonctionArreraNetwork.sortieAjoutEvent(), ""]
@@ -74,8 +75,11 @@ class neuroneTime(neuronBase):
                 return 0
         else :
             return 0
+        """
+        return 0
 
-    def neuronTache(self,requette:str):
+    def __neuronTache(self, requette:str):
+        """
         if ("taches" in requette or "tache" in requette) and "projet" not in requette:
             if ("montre" in requette or "fais voir" in requette):
                 self._listSortie = [self._fonctionArreraNetwork.sortieViewTache(), ""]
@@ -118,3 +122,5 @@ class neuroneTime(neuronBase):
                 return 0
         else:
             return 0
+        """
+        return 0
