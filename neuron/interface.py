cc69d0d696dfa0ffe80784237312a2212d79a243
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2025-11-08 16:22:09 +0100

    AJout de deux nouvelle reponse de l'interface

--- neuron/interface.py
+++ neuron/interface.py
@@ -5,6 +5,12 @@ class interface(neuronBase):
         super().__init__(gestionnaire)
 
     def neurone(self,requette:str):
-        if self._keyword.checkInterface(requette,"noopensoft") :
+        if self._keyword.checkInterface(requette,"erreuropensoft") :
             self._valeurOut = 1
-            self._listSortie = ["noopensoft",""]
+            self._listSortie = [self._language.getPhraseInterfaceOpenSoft("2"),""]
+        elif self._keyword.checkInterface(requette,"noopensoft") :
+            self._valeurOut = 1
+            self._listSortie = [self._language.getPhraseInterfaceOpenSoft("3"),""]
+        elif self._keyword.checkInterface(requette,"opensoft") :
+            self._valeurOut = 1
+            self._listSortie = [self._language.getPhraseInterfaceOpenSoft("1"),""]
