952c4f6e2d85e898562d0131da33796b839da59b
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2025-12-14 18:35:35 +0100

    Ajout que le IA ne vois pas les intruction de quitter

--- neuron/chatBots.py
+++ neuron/chatBots.py
@@ -14,14 +14,15 @@ class neuroneChatbot(neuronBase) :
         #Initilisation des variable nbRand et text et valeur
         self._listSortie = ["", ""]
         self._valeurOut = 0
-        if self.__gestIA.get_ia_is_enable():
-            if self.__gestIA.send_request_ia(requette):
-                if self.__gestIA.get_state_ia_reponse():
-                    self._listSortie = [self.__gestIA.get_reponse_ia(),""]
-                    self._valeurOut = 1
+        if not self._keyword.checkChatBot(requette,"stop"):
+            if self.__gestIA.get_ia_is_enable():
+                if self.__gestIA.send_request_ia(requette):
+                    if self.__gestIA.get_state_ia_reponse():
+                        self._listSortie = [self.__gestIA.get_reponse_ia(),""]
+                        self._valeurOut = 1
+                    else :
+                        self._valeurOut = 0
                 else :
                     self._valeurOut = 0
             else :
                 self._valeurOut = 0
-        else :
-            self._valeurOut = 0
