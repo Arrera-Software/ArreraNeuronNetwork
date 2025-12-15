9b2619c5413c59b275068e90bd6d1f04f8caece0
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2025-12-15 15:17:02 +0100

    Ajout d'un methode pour gerer les grand sortie de l'ia

--- neuron/chatBots.py
+++ neuron/chatBots.py
@@ -1,4 +1,4 @@
-import datetime
+import re
 from neuron.CNeuronBase import *
 
 
@@ -11,6 +11,18 @@ class neuroneChatbot(neuronBase) :
         self.__gestIA = self._gestionnaire.getGestIA()
         self.__gestIA.loadIA()
 
+    def __clear_out_ia(self,texte:str):
+        texte = texte.replace("\n\n", "\n").replace("**","")
+        texte = "\n".join(ligne for ligne in texte.split("\n") if ligne.strip())
+        texte = re.sub(r"\s{2,}", " ", texte)
+        texte = "\n".join(ligne.strip() for ligne in texte.split("\n"))
+
+        nb = len(texte)
+        if nb >= 220:
+            return [17,texte]
+        else :
+            return [1,texte]
+
     def neurone(self,requette:str):
         #Initilisation des variable nbRand et text et valeur
         self._listSortie = ["", ""]
@@ -19,8 +31,14 @@ class neuroneChatbot(neuronBase) :
             if self.__gestIA.get_ia_is_enable():
                 if self.__gestIA.send_request_ia(requette):
                     if self.__gestIA.get_state_ia_reponse():
-                        self._listSortie = [self.__gestIA.get_reponse_ia(),""]
-                        self._valeurOut = 1
+                        out_ia = self.__gestIA.get_reponse_ia()
+                        out_clear = self.__clear_out_ia(out_ia)
+                        if out_clear[0] == 17:
+                            self._gestGUI.activeHelp(out_clear[1])
+                            self._listSortie = [self._language.getPhraseChatBot("1"),""]
+                        else :
+                            self._listSortie = [out_clear[1],""]
+                        self._valeurOut = out_clear[0]
                     else :
                         self._valeurOut = 0
                 else :
