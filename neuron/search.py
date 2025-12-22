40a2bdf139cc87c0071074baa83410373b78c117
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2025-12-22 16:39:07 +0100

    Ajout des instruction pour l'ia sur les fonction de recherche

--- neuron/search.py
+++ neuron/search.py
@@ -9,19 +9,20 @@ class neuroneSearch(neuronBase):
         #Initilisation des variable nbRand et text et valeur
         self._listSortie = ["", ""]
         self._valeurOut = 0
-        if self._keyword.checkSearch(requette,"bigsearch"):
-            search = requette.replace("bigsearch","").replace("grand recherche","").strip()
-            state = self.__fncSearch.bigRecherche(search)
-            if state:
-                self._listSortie = [self._language.getPhraseSearch("1"), ""]
-            else :
-                self._listSortie = [self._language.getPhraseSearch("3"), ""]
-            self._valeurOut = 1
-        elif self._keyword.checkSearch(requette,"search"):
-            search = requette.replace("recherche","").replace("search","").strip()
-            state = self.__fncSearch.search(search)
-            if state:
-                self._listSortie = [self._language.getPhraseSearch("4"), ""]
-            else :
-                self._listSortie = [self._language.getPhraseSearch("2"), ""]
-            self._valeurOut = 1
+        if not self._keyword.checkUtils(requette,"question-fonction"):
+            if self._keyword.checkSearch(requette,"bigsearch"):
+                search = requette.replace("bigsearch","").replace("grand recherche","").strip()
+                state = self.__fncSearch.bigRecherche(search)
+                if state:
+                    self._listSortie = [self._language.getPhraseSearch("1"), ""]
+                else :
+                    self._listSortie = [self._language.getPhraseSearch("3"), ""]
+                self._valeurOut = 1
+            elif self._keyword.checkSearch(requette,"search"):
+                search = requette.replace("recherche","").replace("search","").strip()
+                state = self.__fncSearch.search(search)
+                if state:
+                    self._listSortie = [self._language.getPhraseSearch("4"), ""]
+                else :
+                    self._listSortie = [self._language.getPhraseSearch("2"), ""]
+                self._valeurOut = 1
