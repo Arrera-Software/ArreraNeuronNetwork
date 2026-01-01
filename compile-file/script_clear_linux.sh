78d5b4a0ee90720a6b83f1b4f6979695ffa16773
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2026-01-01 19:06:29 +0100

    Mise a jour du script

--- compile-file/script_clear_linux.sh
+++ compile-file/script_clear_linux.sh
@@ -1,9 +1,3 @@
-pip uninstall -y torch torchvision torchaudio tensorflow tensorflow-cpu llama-cpp-python
-
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
-
-pip install tensorflow-cpu
+pip uninstall -y llama-cpp-python
 
 CMAKE_ARGS="-DGGML_CUDA=off" pip install llama-cpp-python --upgrade --force-reinstall --no-cache-dir --no-binary llama-cpp-python
-
-python -c "import torch; print(torch.cuda.is_available()); import tensorflow as tf; print(tf.config.list_physical_devices('GPU'))"
