3fcdc9f446eadaf6181b52db8f50ae041b58e3f8
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2026-01-26 18:49:35 +0100

    Mise en place de fixe

--- brain/brain.py
+++ brain/brain.py
@@ -194,6 +194,7 @@ class ABrain :
                                         self.__neuronUsed = self.__listNeuron[2]
                                 else :
                                     self.__listOut = self.__gestNeuron.napi.getListSortie()
+                                    print(self.__listOut)
                                     self.__neuronUsed = self.__listNeuron[0]
                             else :
                                 self.__listOut = self.__gestNeuron.nsearch.getListSortie()
--- neuron/API.py
+++ neuron/API.py
@@ -119,6 +119,7 @@ class neuroneAPI(neuronBase) :
                 else:
                     out = self.__fncBreef.summarizeAll()
                     texte = self._language.getPhraseResumerAll("2")
+                    print(texte)
                     if out is not None:
                         outInt = 19
                         self._gestGUI.setGUIActive("resumer",[out,None,outInt])
@@ -126,6 +127,7 @@ class neuroneAPI(neuronBase) :
                         outInt = 20
 
                 if outInt == 12 or outInt == 18 or outInt == 19 or outInt == 5:
+                    print(texte)
                     self._listSortie = [texte,""]
                 else :
                     self._listSortie = [self._language.getPhraseResumerAll("1"),""]
