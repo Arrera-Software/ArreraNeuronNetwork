1b1279760db833ac3cf861639dbe0d714c44f1cd
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2026-07-22 10:16:51 +0200

    Petit update

--- run_test_mac.sh
+++ run_test_mac.sh
@@ -21,25 +21,25 @@ fi
 case $choix in
     1|main)
         echo "🛠️  Compilation de main (ARRERA OPALE)..."
-        pyinstaller --clean compilation/test_main.spec
+        pyinstaller --clean compilation_mac/test_main.spec
         echo "🚀 Lancement de l'application graphique dans le terminal..."
         "./dist/ARRERA OPALE.app/Contents/MacOS/ARRERA OPALE"
         ;;
     2|test-fnc)
         echo "🛠️  Compilation de test-fnc..."
-        pyinstaller --clean compilation/test-fnc.spec
+        pyinstaller --clean compilation_mac/test-fnc.spec
         echo "🚀 Lancement de test-fnc dans le terminal..."
         ./dist/test-fnc.app/Contents/MacOS/test-fnc
         ;;
     3|test-gui)
         echo "🛠️  Compilation de test-gui..."
-        pyinstaller --clean compilation/test-gui.spec
+        pyinstaller --clean compilation_mac/test-gui.spec
         echo "🚀 Lancement de l'application graphique test-gui dans le terminal..."
         "./dist/Test-GUI.app/Contents/MacOS/Test-GUI"
         ;;
     4|test-user-setting)
         echo "🛠️  Compilation de test-user-setting..."
-        pyinstaller --clean compilation/test-user-setting.spec
+        pyinstaller --clean compilation_mac/test-user-setting.spec
         echo "🚀 Lancement de test-user-setting dans le terminal..."
         ./dist/Test-User-Setting.app/Contents/MacOS/Test-User-Setting
         ;;
