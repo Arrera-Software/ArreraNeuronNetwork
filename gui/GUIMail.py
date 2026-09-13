a5fdf1245a0ced2b6916a3f0b784744b923663d3
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2026-09-13 15:23:37 +0200

    Suppression d'un bouton

--- gui/GUIMail.py
+++ gui/GUIMail.py
@@ -45,6 +45,7 @@ class GUIMail(GuiBase) :
         self.__redaction_frame.grid_rowconfigure(1, weight=0)
         self.__redaction_frame.grid_rowconfigure(2, weight=0)
         self.__redaction_frame.grid_rowconfigure(3, weight=1)
+        self.__redaction_frame.grid_rowconfigure(4, weight=0)
 
         # Configuration de corrector_frame
         self.__corrector_frame.grid_columnconfigure(0, weight=1)
@@ -52,6 +53,7 @@ class GUIMail(GuiBase) :
         self.__corrector_frame.grid_rowconfigure(1, weight=0)
         self.__corrector_frame.grid_rowconfigure(2, weight=0)
         self.__corrector_frame.grid_rowconfigure(3, weight=1)
+        self.__corrector_frame.grid_rowconfigure(4, weight=0)
 
         # Configuration de reponse_frame
         self.__reponse_frame.grid_columnconfigure(0, weight=1)
@@ -61,6 +63,7 @@ class GUIMail(GuiBase) :
         self.__reponse_frame.grid_rowconfigure(3, weight=1)
         self.__reponse_frame.grid_rowconfigure(4, weight=0)
         self.__reponse_frame.grid_rowconfigure(5, weight=1)
+        self.__reponse_frame.grid_rowconfigure(6, weight=0)
 
         # Configuration de out_frame
         out_frame.grid_columnconfigure(0, weight=1)
@@ -71,7 +74,7 @@ class GUIMail(GuiBase) :
         out_frame.grid_rowconfigure(4, weight=1)
 
         bottom_frame.grid_rowconfigure(0, weight=1)
-        for i in range(4):
+        for i in range(3):
             bottom_frame.grid_columnconfigure(i, weight=1, uniform="bottom_btns")
 
         # Placement des frames principales
@@ -91,7 +94,6 @@ class GUIMail(GuiBase) :
         btn_reponse = aButton(top_frame, text="Réponse", command=self.__show_reponse)
 
         btn_clear = aButton(bottom_frame, text="Effacer tout")
-        btn_generate = aButton(bottom_frame, text="Générer")
         btn_copie_body = aButton(bottom_frame, text="Copier le corps")
         btn_copy_all = aButton(bottom_frame, text="Copier tout")
 
@@ -110,6 +112,7 @@ class GUIMail(GuiBase) :
         l_t_consigne_redaction = aLabel(self.__redaction_frame, text="Informations clés :", police_size=15)
         self.__t_write_consigne = aTextScrollable(self.__redaction_frame)
         self.__t_write_consigne.enableTextBox()
+        btn_generate_redaction = aButton(self.__redaction_frame, text="Générer")
 
         # Correction
         l_title_correction = aLabel(self.__corrector_frame, text="Correction", police_size=30)
@@ -117,6 +120,7 @@ class GUIMail(GuiBase) :
         l_t_correction_redaction = aLabel(self.__corrector_frame, text="Corp du mail :", police_size=15)
         self.__t_write_correction = aTextScrollable(self.__corrector_frame)
         self.__t_write_correction.enableTextBox()
+        btn_generate_correction = aButton(self.__corrector_frame, text="Corriger")
 
         # Reponse
         l_title_reponse = aLabel(self.__reponse_frame, text="Réponse à un mail", police_size=30)
@@ -127,6 +131,7 @@ class GUIMail(GuiBase) :
         l_t_reponse_consigne = aLabel(self.__reponse_frame, text="Informations clés :", police_size=15)
         self.__t_write_reponse_consigne = aTextScrollable(self.__reponse_frame)
         self.__t_write_reponse_consigne.enableTextBox()
+        btn_generate_reponse = aButton(self.__reponse_frame, text="Répondre")
 
         # Placement des widget de top et bottom
         btn_redaction.grid(row=0, column=0, sticky="ew", padx=15, pady=5)
@@ -134,21 +139,22 @@ class GUIMail(GuiBase) :
         btn_reponse.grid(row=0, column=2, sticky="ew", padx=15, pady=5)
 
         btn_clear.grid(row=0, column=0, sticky="ew", padx=10, pady=5)
-        btn_generate.grid(row=0, column=1, sticky="ew", padx=10, pady=5)
-        btn_copie_body.grid(row=0, column=2, sticky="ew", padx=10, pady=5)
-        btn_copy_all.grid(row=0, column=3, sticky="ew", padx=10, pady=5)
+        btn_copie_body.grid(row=0, column=1, sticky="ew", padx=10, pady=5)
+        btn_copy_all.grid(row=0, column=2, sticky="ew", padx=10, pady=5)
 
         # Placement des widgets dans redaction_frame
         l_title_redaction.grid(row=0, column=0, sticky="w", padx=10, pady=(5, 10))
         self.__e_redaction_objet.grid(row=1, column=0, sticky="ew", padx=10, pady=(5, 10))
         l_t_consigne_redaction.grid(row=2, column=0, sticky="w", padx=10, pady=(5, 2))
         self.__t_write_consigne.grid(row=3, column=0, sticky="nsew", padx=10, pady=(0, 10))
+        btn_generate_redaction.grid(row=4, column=0, sticky="ew", padx=10, pady=(0, 10))
 
         # Placement des widgets dans corrector_frame
         l_title_correction.grid(row=0, column=0, sticky="w", padx=10, pady=(5, 10))
         self.__e_correction_objet.grid(row=1, column=0, sticky="ew", padx=10, pady=(5, 10))
         l_t_correction_redaction.grid(row=2, column=0, sticky="w", padx=10, pady=(5, 2))
         self.__t_write_correction.grid(row=3, column=0, sticky="nsew", padx=10, pady=(0, 10))
+        btn_generate_correction.grid(row=4, column=0, sticky="ew", padx=10, pady=(0, 10))
 
         # Placement des widgets dans reponse_frame
         l_title_reponse.grid(row=0, column=0, sticky="w", padx=10, pady=(5, 10))
@@ -157,6 +163,7 @@ class GUIMail(GuiBase) :
         self.__t_write_reponse_mail.grid(row=3, column=0, sticky="nsew", padx=10, pady=(0, 5))
         l_t_reponse_consigne.grid(row=4, column=0, sticky="w", padx=10, pady=(5, 2))
         self.__t_write_reponse_consigne.grid(row=5, column=0, sticky="nsew", padx=10, pady=(0, 10))
+        btn_generate_reponse.grid(row=6, column=0, sticky="ew", padx=10, pady=(0, 10))
 
         # Placement des widgets dans out_frame
         l_title_out.grid(row=0, column=0, sticky="w", padx=10, pady=(5, 10))
